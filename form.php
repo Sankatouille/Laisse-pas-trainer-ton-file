c91a120d8e322cb1d30333ed46c4f661cffb58c7
Sankatouille <affile.benjamin@gmail.com>
2021-11-07 14:07:32 +0100

    oczi

---
+++ form.php
@@ -0,0 +1,30 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === "POST"){ 
+    
+    $uploadDir = 'uploads/';
+    
+    $uploadFile = $uploadDir . basename($_FILES['avatar']['name']);
+    $extension = pathinfo($_FILES['avatar']['name'], PATHINFO_EXTENSION);
+    $authorizedExtensions = ['jpg','png','gif','webp'];
+    $maxFileSize = 1000000;
+    
+
+    if( (!in_array($extension, $authorizedExtensions))){
+        $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
+    }
+
+    if( file_exists($_FILES['avatar']['tmp_name']) && filesize($_FILES['avatar']['tmp_name']) > $maxFileSize)
+    {
+    $errors[] = "Votre fichier doit faire moins de 1M !";
+    }
+}
+
+?>
+
+
+<form method="post" enctype="multipart/form-data">
+    <label for="imageUpload">Upload an profile image</label>    
+    <input type="file" name="avatar" id="imageUpload" />
+    <button name="send">Send</button>
+</form>
